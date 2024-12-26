95a2ecb8895f65e5e6f2f2939a90a60a9bdcff1d
Remington Steed <8701105+remocrevo@users.noreply.github.com>
2024-12-26 17:12:32 -0500

    Create __init__.py for submission_review

---
+++ submission_review/__init__.py
@@ -0,0 +1,10 @@
+# submission_review/__init__.py
+from flask import Blueprint
+
+# Create the blueprint
+submissions_bp = Blueprint('submissions', __name__,
+    url_prefix='/find_reviewed.py',
+    template_folder='templates/submission_review')
+    
+# Import routes at the bottom to avoid circular imports
+from . import routes
