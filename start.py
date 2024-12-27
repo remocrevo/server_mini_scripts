65bc42fcf3b8e432abca781dfb818812ab2e45a7
Remington Steed <8701105+remocrevo@users.noreply.github.com>
2024-12-27 17:19:44 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,13 @@
+import asyncio
 from hypercorn.config import Config
-from hypercorn.run import run
+from hypercorn.asyncio import serve
 from main import app
 import os
 
-if __name__ == "__main__":
-    config = Config()
-    config.bind = [f"0.0.0.0:{int(os.environ.get('PORT', 10000))}"]
-    config.workers = 4
-    config.timeout = 300
-    config.graceful_timeout = 300
-    config.keep_alive_timeout = 300
-    
-    # Pass config as a keyword argument
-    run("main:app", config_path=config)
+config = Config()
+config.bind = [f"0.0.0.0:{int(os.environ.get('PORT', 10000))}"]
+config.timeout = 300
+config.graceful_timeout = 300
+config.keep_alive_timeout = 300
+
+asyncio.run(serve(app, config))
