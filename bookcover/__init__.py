553960c196c12e6bca587a599c057a334e56d4e4
Remington Steed <8701105+remocrevo@users.noreply.github.com>
2025-01-01 21:44:43 -0500

    Bookcover app: Create __init__.py

---
+++ bookcover/__init__.py
@@ -0,0 +1,10 @@
+# bookcover/__init__.py
+from flask import Blueprint
+
+# Create the blueprint
+submissions_bp = Blueprint('bookcover', __name__,
+    url_prefix='/bookcover',
+    template_folder='templates/bookcover')
+    
+# Import routes at the bottom to avoid circular imports
+from . import routes
